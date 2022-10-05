65028ab53af3d4169ddfd6899fe4dda89a0d016e
Marie-Ange Pesant <marieangepesant@hotmail.com>
2022-10-05 13:01:09 -0400

    after class 5

--- YCIT025 - Reading and Debugging Javascript/Class 5/reduce2.js
+++ YCIT025 - Reading and Debugging Javascript/Class 5/reduce2.js
@@ -4,7 +4,19 @@ const r1 = [1,5,3,6,2];
 
 let total = r1.reduce((acc, val) => { return acc + val; }, 0);
 
-let largest = r1.reduce((acc, val) => { return 0;}, r1[0] )
+/* let largest = r1.reduce((acc, val) => { 
+    if (acc > val)  {
+        return acc ;}
+        else {
+            return val;}
+            }, r1[0] )
+*/
+
+// largest = r1.reduce((acc, val) => {return Math.max(acc, val)});
+
+let largest = r1.reduce((acc, val) => acc > val ? acc : val);
+
+console.log(largest);
 
 console.log(total);
 
@@ -15,4 +27,6 @@ const smallerThanTen = inputSet.filter(w => w < 10);
 
 console.log(smallerThanTen)
 
-const words = ['spray', 'limit', 'elite', 'exuberant']
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+const result = words.filter(word => word.length > 6);
+console.log(result);
